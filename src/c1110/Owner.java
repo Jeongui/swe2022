e89af0ef801b78a00f8acd798126cb906d294e08
Jeongui <leju6269@naver.com>
2017-11-10 14:01:49 +0900

    Object 클래스 메서드

--- src/c1110/Owner.java
+++ src/c1110/Owner.java
@@ -1,5 +1,24 @@
 package c1110;
 
 class Owner {
+    private final int ownerValue;
+    private final Base ownerBase;
 
+    Owner(int ownerValue, Base ownerBase) {
+        this.ownerValue = ownerValue;
+        this.ownerBase = ownerBase;
+    }
+
+    @Override
+    protected Owner clone() throws CloneNotSupportedException { // 인스턴스 유일성 보장되어야할 경우 스루
+        //얕은복사
+        Owner result = new Owner(this.ownerValue, this.ownerBase);
+        //깊은복사
+        result = new Owner(this.ownerValue, this.ownerBase.clone());
+        return result;
+    }
+
+    public Base getBase(){
+        return ownerBase;
+    }
 }
