0a4387254fe08c539bfd36733de672df4524c3b2
Jeongui <leju6269@naver.com>
2017-11-20 19:52:41 +0900

    기한 설정, 미리 알림

--- src/ToDo/ToDoTask.java
+++ src/ToDo/ToDoTask.java
@@ -1,47 +1,41 @@
 package ToDo;
 
+import java.sql.Time;
 import java.util.Date;
 
 public class ToDoTask implements Comparable<ToDoTask>{
     private String taskname;
     private Date deadline;
     private boolean done;
+    private Time alarmTime;
 
 
     public ToDoTask(String taskname) {
         this.taskname = taskname;
         this.deadline = new Date();
         this.done = false;
+        this.alarmTime = new Time();
     }
 
     public void setTaskname(String settingname) {
         this.taskname = settingname;
-    } // 할 일
+    }
 
     public void setDeadline(int year1, int month1, int date1){
         this.deadline.setYear(year1);
         this.deadline.setDate(date1);
         this.deadline.setMonth(month1);
-    } //기한 설정
+    }
 
     public void setDone(boolean status){
         this.done = status;
-    } // 완료 여부 확인
-
-    public String getTaskname() {
-        return this.taskname;
     }
 
-    public String getDeadline() {
-        int year1 = deadline.getYear();
-        int month1 = deadline.getMonth();
-        int date1 = deadline.getDate();
-        String yyyymmdd = year1 + "/" + month1 + "/" + date1;
-        return yyyymmdd;
-    }
-
-    public Boolean getDone() {
-        return this.done;
+    public void isAlarm(Time alarm){
+        this.alarmTime = alarm;
+        if (Time == alarmTime) {
+            return true;
+        }
     }
 
     @Override
