e37e45354e1cfe97c48f552877e2bab73eb43716
Jeongui <leju6269@naver.com>
2017-11-26 17:41:59 +0900

    list 입력 받아서 출력하기

--- src/ToDo/App.java
+++ src/ToDo/App.java
@@ -1,24 +1,46 @@
 package ToDo;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
 
 public class App {
+    ArrayList <ToDoList> todoLists;
 
-    private String newName;
-
-    public App (String listName){
-        this.newName = listName;
+    //생성자
+    public App(){
+        this.todoLists = new ArrayList<>();
     }
 
-    public void resetListName{
-        ToDoList.listName = null;
-        ToDoList.listName = newName;
+    public void addList(ToDoList newList){
+        todoLists.add(newList);
+
     }
 
-    public void addList{
-        ToDoList toDoList1 = new ToDoList();
+    public void removeList(ToDoList list){
+        if (!(todoLists.isEmpty())
+                && todoLists.contains(list))
+            todoLists.remove(list);
     }
 
-    public void addTask{
-        ToDoTask toDoTask1 = new ToDoTask();
+    public String getList(){
+        if (todoLists.isEmpty()) return "리스트를 추가해 주십시오";
+        else return todoLists.toString();
     }
+
+    //3번과제
+    public void list (String list){
+        Scanner in = new Scanner(list).useDelimiter("\\n");
+        System.out.println("Set the list name");
+        String listName = in.nextLine();
+        System.out.println("Set the deadline");
+        int deadline = in.nextInt();
+        System.out.println("Check the alarm");
+        boolean isAlarm = in.hasNext();
+        if (isAlarm = true) return (listName, deadline, isAlarm);
+        return (listName, deadline);
+    }
+
+
 }
